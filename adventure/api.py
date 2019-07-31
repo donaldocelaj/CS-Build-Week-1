7d6eba8bc951f839f444b62907edec2e7c9fcf76
donaldocelaj <41174917+donaldocelaj@users.noreply.github.com>
2019-07-31 09:19:08 -0400

    Updating post requests

--- adventure/api.py
+++ adventure/api.py
@@ -60,6 +60,7 @@ def move(request):
         return JsonResponse({'name':player.user.username, 'title':room.title, 'description':room.description, 'players':players, 'error_msg':"You cannot move that way."}, safe=True)
 
 
+    
 @csrf_exempt
 @api_view(["POST"])
 def say(request):
