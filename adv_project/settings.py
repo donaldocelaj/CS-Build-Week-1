4157048e0fcf155258be75047a21c870233203b1
Donaldo Celaj <donaldocelaj@gmail.com>
2019-08-01 23:39:51 -0400

    pushing working api

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = '1fo$xhy+vnsw7a2zz1eu@3%5zj$ya6f1q1qpjqbk!&4hl6&u97'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
